5d431387c00f833537f95c7ce3dad83302530bf9
jordaniza <jordan.imran@predixa.ai>
2022-01-09 14:06:22 +0400

    added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -3,56 +3,67 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+const Hero = (): JSX.Element => (
+  <section>
+    <h1>Simple, High Yield DeFi</h1>
+    <section className="actions">
+      <button>Get Started</button>
+      <button>Learn More</button>
+    </section>
+  </section>
+)
+
+type Vault = {
+  name: string;
+  symbol: string;
+  icon?: string;
+  description: string;
+  network: string;
+  stats: {
+    APY: number;
+  }
+}
+
+const Vault = ({ vault }: { vault: Vault }): JSX.Element => (
+  <div className="border-red-100 border-2">
+    <h2 className="underline">{ vault.name }</h2>
+    <p>{ vault.description }</p>
+    <div className="details">
+      <p>APY: {vault.stats.APY}</p>
+    </div>
+    <section>
+      <button>Deposit</button>
+      <button>Withdraw</button>
+    </section>
+  </div>
+) 
+
+const VaultList = ({ vaults }: { vaults: Vault[] }): JSX.Element => (
+  <>
+    {vaults.map(v => (<Vault key={v.symbol} vault={v}/>))}
+  </>
+)
+
 function App() {
+  const data: Vault[] = [{
+    symbol: 'USDC',
+    name: 'StableCoin USDC',
+    description: 'High yield stablecoin farming from USDC',
+    network: 'Polygon',
+    stats: {
+      APY: 10.2
+    } 
+  }];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <header>Header goes here, inc. connect wallet stuff</header>
+      <Hero />
+      <section>
+        <h1>Main Mono Content goes here</h1>
+        <VaultList vaults={data} />
+      </section>
     </div>
   );
-}
+} 
 
 export default App;
